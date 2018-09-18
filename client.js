fe664ee67b7a9a3ebbdfc721e399afafb3f13264
Matt Everett <meverett@mediocre.com>
2018-09-18 14:55:41 -0700

    finish up intial client work

--- client.js
+++ client.js
@@ -13,6 +13,145 @@ function NewgisticsClient(args) {
         secret: ''
     }, args);
 
+    _this.authenticate = function(callback) {
+        var token = cache.get('newgistics_auth_token');
+        if (token !== null) {
+            return callback(null, token);
+        }
+
+        var authRequest = {
+            formData: {
+                client_id: _this.opts.id,
+                client_secret: _this.opts.secret,
+                grant_type: 'client_credentials',
+                scope: 'api'
+            },
+            method: 'POST',
+            url: _this.opts.auth_url
+        };
+
+        request(authRequest, function(err, res, body) {
+            if (err) {
+                return callback(err);
+            }
+
+            var jsonBody = JSON.parse(body);
+            const expiresIn = Number(jsonBody.expires_in);
+            token = jsonBody.access_token;
+
+            if (!token || !expiresIn) {
+                return callback(new Error('Unable to authenticate with Newgistics (token or expiry not received)'));
+            }
+
+            cache.put('newgistics_auth_token', token, expiresIn / 2);
+            return callback(null, token);
+        });
+    };
+
+    _this.createShipment = function(address, length, width, height, weight, callback) {
+        if (!address || !address.name || !address.line1 || !address.postalCode || !address.city || !address.state) {
+            return callback(new Error('Address object missing required properties (name, line1, city, state, postalCode are required)'));
+        }
+
+        if (!length || !width || !height || !weight) {
+            return callback(new Error('Length, width, height and weight are required'));
+        }
+
+        _this.authenticate(function(err, token) {
+            if (err) {
+                return callback(err);
+            }
+
+            var shortestTwoDims = [ Number(length), Number(width), Number(height) ].sort((a, b) => b - a).slice(-2);
+            var girth = (shortestTwoDims[0] + shortestTwoDims[1]) * 2;
+
+            var rateRequestData = {
+                PricePackage: true,
+                clientFacilityId: _this.opts.clientFacilityId,
+                ngsFacilityId: _this.opts.facilityId,
+                returnAddress: {
+                    name: _this.opts.returnAddress.name,
+                    address1: _this.opts.returnAddress.line1,
+                    address2: _this.opts.returnAddress.line2,
+                    city: _this.opts.returnAddress.city,
+                    stateOrProvince: _this.opts.returnAddress.state,
+                    postalCode: _this.opts.returnAddress.postalCode,
+                    country: _this.opts.returnAddress.country || 'US',
+                    isResidential: _this.opts.returnAddress.isResidential || false
+                },
+                additionalServices: [
+                    'DeliveryConfirmation'
+                ],
+                returnService: 'AddressServiceRequested',
+                classOfService: 'Ground',
+                referenceNumbers: [],
+                labelFormat: 'ZPL',
+                verifyAddress: true,
+                correctAddress: false,
+                hazmatClasses: []
+            };
+
+            rateRequestData.dimensions = {
+                length: {
+                    unitOfMeasure: 'Inches',
+                    measurementValue: length.toString()
+                },
+                width: {
+                    unitOfMeasure: 'Inches',
+                    measurementValue: width.toString()
+                },
+                height: {
+                    unitOfMeasure: 'Inches',
+                    measurementValue: height.toString()
+                },
+                girth: {
+                    unitOfMeasure: 'Inches',
+                    measurementValue: girth.toString()
+                },
+                isRectangular: true
+            },
+
+            rateRequestData.shipToAddress = {
+                name: address.name,
+                address1: address.line1,
+                city: address.city,
+                stateOrProvince: address.state,
+                postalCode: address.postalCode,
+                country: address.country || 'US',
+                isResidential: address.isResidential || false
+            };
+
+            rateRequestData.weight = {
+                unitOfMeasure: 'Pounds',
+                measurementValue: weight.toFixed(2)
+            };
+
+            var rateRequest = {
+                auth: {
+                    bearer: token
+                },
+                json: rateRequestData,
+                method: 'POST',
+                url: _this.opts.api_url
+            };
+
+            request(rateRequest, function(err, res, body) {
+                if (err) {
+                    return callback(err);
+                }
+
+                //console.log(token);
+                console.log(JSON.stringify(body));
+
+                if (!body || !body.data) {
+                    return callback(new Error('No response received from Newgistics'));
+                }
+
+                callback(null, body.data);
+            });
+        });
+    };
+
     return _this;
 }
 
